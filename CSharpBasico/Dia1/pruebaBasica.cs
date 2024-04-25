4ae1c756b9482b8ea274852a7c3f1562869d8ddc
Darwin Sanchez <dscdeveloper@outlook.com>
2024-04-24 22:34:32 -0700

    dia 202404224

--- CSharpBasico/Dia1/pruebaBasica.cs
+++ CSharpBasico/Dia1/pruebaBasica.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CSharpBasico.Dia3.darwin;
 
 namespace CSharpBasico.Dia1
 {
@@ -22,6 +23,10 @@ namespace CSharpBasico.Dia1
         private void btnEnviarSaludo_Click(object sender, EventArgs e)
         {
             lblSaludo.Text = tbxNombre.Text;
+
+            EjemploCampo clase1 = new EjemploCampo();
+
+
             
         }
 
---
+++ CSharpBasico/Dia3/EjemploCampo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpBasico.Dia3.darwin
+{
+    public class EjemploCampo
+    {
+        //campo codigo
+        private string codigo = "123456";
+        
+
+
+        public void Hola() {
+
+            //MessageBox.Show(codigo);
+
+            //variable local codigo
+            string codigo = "11111";
+
+            MessageBox.Show(codigo);
+            MessageBox.Show(this.codigo);
+
+
+        }
+
+
+        public void Adios()
+        {
+
+            MessageBox.Show(codigo);
+
+        }
+
+
+
+    }
+}
