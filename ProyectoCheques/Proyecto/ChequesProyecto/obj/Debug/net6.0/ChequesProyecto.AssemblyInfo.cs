326a740109c66baceea9580c3aeb10b7c96a8039
darwinrsc <dscdeveloper@outlook.com>
2025-07-06 11:02:36 -0500

    dsc: docker

--- ProyectoCheques/Proyecto/ChequesProyecto/obj/Debug/net6.0/ChequesProyecto.AssemblyInfo.cs
+++ ProyectoCheques/Proyecto/ChequesProyecto/obj/Debug/net6.0/ChequesProyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChequesProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670ccf23543523ec4fd181e1405f48eed1f6a4bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61e13fd7257a1aeae0cb26fa58f527adb043169")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChequesProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChequesProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
